caaa85c4cb2f2a60daf287f92c700082f81c4768
FJ Tech <164685231+FJtechApp@users.noreply.github.com>
2024-05-19 22:55:52 -0300

    Delete fj-tech directory

--- fj-tech/app/estandes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function EstandesPage() {
-	return (
-		<div>
-			<h1 className={title()}>Localização dos Estandes</h1>
-		</div>
-	);
-}
