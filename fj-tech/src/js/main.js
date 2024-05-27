d6fc5647761a35a153911866fc78680a1275d92d
Bianca Silva <biancaflorianodasilva@gmail.com>
2024-05-27 14:29:49 +0000

    Navegação básica

--- fj-tech/src/js/main.js
+++ fj-tech/src/js/main.js
@@ -7,3 +7,7 @@ import Alert from 'bootstrap/js/dist/alert'
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap'
+
+// Import icons
+import '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-free'
