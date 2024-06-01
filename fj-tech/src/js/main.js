e665b1daf8f64088fe91e519561fdf5cc5107ce5
Bianca Silva <biancaflorianodasilva@gmail.com>
2024-06-01 20:21:02 +0000

    Dark-mode e perfil do voluntários

--- fj-tech/src/js/main.js
+++ fj-tech/src/js/main.js
@@ -1,5 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+import './color-modes.js'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
@@ -7,9 +8,6 @@ import Alert from 'bootstrap/js/dist/alert'
 
 const alertList = document.querySelectorAll('.alert')
 const alerts = [...alertList].map(element => new bootstrap.Alert(element))
-
-// or, specify which plugins you need:
-import { Tooltip, Toast, Popover } from 'bootstrap'
 
 // Import icons
 import '@fortawesome/fontawesome-svg-core'
@@ -21,25 +19,11 @@ import '@fortawesome/fontawesome-free'
  */
 const validaAcessoAdministrativo = document.getElementById('#validaAcessoAdministrativo')
 if (validaAcessoAdministrativo.querySelector('.was-validated')) {
-   // Valida valor informado e habilita o botão de acesso
+  // Valida valor informado e habilita o botão de acesso
 }
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
 
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
-}
+/**
+ * Uso da API ViaCEP para preenchimento automático do endereço
+ * https://viacep.com.br/
+ */
