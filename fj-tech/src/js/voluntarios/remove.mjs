92370944ef73239c3abc2612cb7e3b5ada645965
Bianca Silva <biancaflorianodasilva@gmail.com>
2024-07-07 19:43:21 +0000

    fix no bug de remoção de voluntário da lista

--- fj-tech/src/js/voluntarios/remove.mjs
+++ fj-tech/src/js/voluntarios/remove.mjs
@@ -15,18 +15,15 @@ document.getElementById("btnRemoverVoluntario").addEventListener("click", async 
     try {
         await deleteDoc(doc(db, "voluntario", idVoluntarioRemocao));
         console.log("Voluntário removido com sucesso!");
+
+        /**
+         * Atualiza a listagem de voluntários após a remoção de um voluntário
+         */
+        const voluntarioRemovido = document.getElementById(idVoluntarioRemocao);
+        if (voluntarioRemovido) {
+            voluntarioRemovido.remove();
+        }
     } catch (error) {
         console.error("Erro ao remover voluntário: ", error);
     }
-});
-
-
-/**
- * Atualiza a listagem de voluntários após a remoção de um voluntário
- */
-document.getElementById("corpoTabelaDeListagemDeVoluntarios").addEventListener("click", async (event) => {
-    const identificaVoluntarioRemovido = event.target.closest("tr").id;
-
-    // Remove o voluntário da lista assim que o modal de confirmação de exclusão é fechado
-    document.getElementById(identificaVoluntarioRemovido).remove();
 });
