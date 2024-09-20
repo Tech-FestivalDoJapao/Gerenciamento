b88cac75400e10a1fc0a10b3e7ceb9bf1fdf3239
FJ Tech <164685231+Tech-FestivalDoJapao@users.noreply.github.com>
2024-09-20 19:58:38 -0300

    Update fj-tech/src/js/voluntarios/festival/resgate_voucher.mjs

--- fj-tech/src/js/voluntarios/festival/resgate_voucher.mjs
+++ fj-tech/src/js/voluntarios/festival/resgate_voucher.mjs
@@ -52,7 +52,7 @@ document.getElementById("corpoTabelaDeListagemDeVoluntarios").addEventListener("
             // Exibe o tipo de voucher resgatado pelo voluntário
             const identificaVoucherResgatado = document.createElement('small');
             identificaVoucherResgatado.className = 'opacity-50 m-0 px-4';
-            smallElement.textContent = `Voucher resgatado: ${tipoVoucherDoVoluntario}`;
+            identificaVoucherResgatado.textContent = `Voucher resgatado: ${tipoVoucherDoVoluntario}`;
             txtResgateVoucher.appendChild(smallElement);
             bloquearResgateVoucher();
         }).catch((erro) => {
