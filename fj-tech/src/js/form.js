35760a3a72289d619b88a9742f5a17d71809b4f2
Bianca Silva <biancaflorianodasilva@gmail.com>
2024-09-25 14:30:15 +0000

    Inputs das fichas de cadastro

--- fj-tech/src/js/form.js
+++ fj-tech/src/js/form.js
@@ -8,3 +8,31 @@ import './fichas/cadastra_voluntario.mjs'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+/**
+ * Personalização dos inputs do formulário
+ */
+document.querySelectorAll("input").forEach(input => {
+    input.classList.add("border-0", "border-bottom", "border-danger", "border-opacity-50");
+    input.addEventListener("focus", () => {
+        input.classList.add("border-opacity-75");
+    });
+
+    if (input.type === "checkbox" || input.type === "radio") {
+        input.classList.remove("border-0");
+        input.classList.add("border", "border-opacity-75");
+    } 
+});
+
+/**
+ * Personalização dos selects do formulário
+ */
+document.querySelectorAll("select").forEach(select => {
+    select.classList.add("border-0", "border-bottom", "border-danger", "border-opacity-50");
+});
+
+/**
+ * Habilita o uso de tooltips do boostatrap no sistema
+ */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
